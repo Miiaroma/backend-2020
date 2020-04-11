c4dc2cca32632674314b18ae16540e994dd1a368
Miiaroma <miia.romanainen@gmail.com>
2020-04-12 00:11:15 +0300

    Service layer added.

---
+++ CurrencyApi/CurrencyApi/Services/IRateService.cs
@@ -0,0 +1,16 @@
+﻿using CurrencyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApi.Services
+{
+    public interface IRateService
+    {
+        RATE Create(RATE rate);
+        List<RATE> Read();
+        RATE Read(string id);
+        RATE Update(string id, RATE rate);
+    }
+}
---
+++ CurrencyApi/CurrencyApi/Services/RateService.cs
@@ -0,0 +1,50 @@
+﻿using CurrencyApi.Models;
+using CurrencyApi.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApi.Services
+{
+    public class _RateService : IRateService
+    {
+        private readonly IRateRepository _rateRepository;
+
+        public _RateService(IRateRepository rateRepository)
+        {
+            _rateRepository = rateRepository;
+        }
+
+        public RATE Create(RATE rate)
+        {
+            return _rateRepository.Create(rate);
+        }
+
+        public List<RATE> Read()
+        {
+            var rates = _rateRepository.Read();
+            return rates;
+        }
+
+        public RATE Read(string id)
+        {
+            var rate = _rateRepository.Read(id);
+            return rate;           
+        }
+
+        public RATE Update(string id, RATE rate)
+        {
+            var updatedPerson = _rateRepository.Read(id);
+            if (updatedPerson == null)
+            {
+                throw new Exception("Rate not found!");
+            }
+            else
+            {
+                return _rateRepository.Update(rate);
+            }
+            
+        }
+    }
+}
