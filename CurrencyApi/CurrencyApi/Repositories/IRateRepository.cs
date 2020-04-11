5ba30b7c2052adb8ec910db4607e5eea798febdc
Miiaroma <miia.romanainen@gmail.com>
2020-04-11 23:35:59 +0300

    Repositories added.

---
+++ CurrencyApi/CurrencyApi/Repositories/IRateRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CurrencyApi.Models;
+
+namespace CurrencyApi.Repositories
+{
+    public interface IRateRepository
+    {
+        RATE Create(RATE rate);
+        List<RATE> Read();
+        RATE Read(string id);
+        RATE Update(RATE rate);
+    }
+}
---
+++ CurrencyApi/CurrencyApi/Repositories/RateRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CurrencyApi.Data;
+using CurrencyApi.Models;
+
+
+namespace CurrencyApi.Repositories
+{
+    public class _RateRepository : IRateRepository
+    {
+        private readonly Dtbankdb1Context _dtbankdb1Context;
+
+        public _RateRepository(Dtbankdb1Context dtbankdb1Context)
+        {
+            _dtbankdb1Context = dtbankdb1Context;
+        }
+
+        public RATE Create(RATE rate)
+        {
+            _dtbankdb1Context.RATE.Add(rate);
+            _dtbankdb1Context.SaveChanges();
+            return rate;            
+        }
+
+        public List<RATE> Read()
+        {
+            var rates = _dtbankdb1Context.RATE                 
+                 .ToList();
+            return rates;
+        }
+
+        public RATE Read(string id)
+        {
+            var rate = _dtbankdb1Context.RATE                
+                .AsNoTracking().FirstOrDefault(p => p.Id == id);
+            return rate;            
+        }
+
+        public RATE Update(RATE rate)
+        {
+            _dtbankdb1Context.Update(rate);
+            _dtbankdb1Context.SaveChanges();
+            return rate;
+        }
+    }
+}
